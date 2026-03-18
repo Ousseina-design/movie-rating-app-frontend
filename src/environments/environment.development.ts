249cbd5766b4584cac611c5f9f5b217d526a41f6
Abdou Habi Ousseina <abdouhabiousseina@gmail.com>
2026-03-18 11:00:04 +0100

    movie rating frontend

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production:false,
+    contractAddress: '0x994e8D8cE7EC76Ad8C9BCf9eE44959B32719C31b',
+    chainId: '0x539',
+    ownerAddress:''
+};
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production:true,
+    contractAddress: '0x0F5D0d682d510ef6f5e2FaBCF30A30fc235433e2',
+    chainId: '0xaa36a7',
+    ownerAddress:''
+};
+
